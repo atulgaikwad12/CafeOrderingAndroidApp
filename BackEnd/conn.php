2d9d78a0446ef07c42f5c33600eb2d4d46bb51c9
atulgaikwad12 <atulanilgaikwad12@gmail.com>
2022-02-14 00:52:40 +0530

    connection file
    removed actual credentials from file 
    Now need to replace dbname , username , password in this file
     to connect MySQL database

---
+++ BackEnd/conn.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "localhost";
+$username = "username";
+
+$password = "password";
+
+$dbname = "dbname";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// if ($conn->connect_error) {
+//     // die("Connection failed: " . $conn->connect_error);
+//     // echo "". $conn->connect_error;
+// }
+
+
+?>
